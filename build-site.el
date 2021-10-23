f0cd32723cb2248a36411981cfc260d297e54b4c
vaporinsky <root@ip-172-31-26-107.us-west-2.compute.internal>
2021-10-23 15:08:06 +0000

    TT

---
+++ build-site.el
@@ -0,0 +1,48 @@
+(require 'package)                                                              
+(setq package-archives                                                          
+      '(("gnu" . "https://elpa.gnu.org/packages/")                              
+        ("melpa-stb" . "https://stable.melpa.org/packages/")                    
+        ("melpa" . "https://melpa.org/packages/"))                              
+      tls-checktrust t                                                          
+      tls-program '("gnutls-cli --x509cafile %t -p %p %h")                      
+      gnutls-verify-error t)
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(package-install 'htmlize)
+
+
+
+
+
+
+ 
+
+(require 'ox-publish)
+
+(setq org-html-validation-link nil
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style nil
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" /> ")
+
+(setq org-publish-project-alist
+      (list
+       (list "my-org-site"
+	     :recursive t
+	     :base-directory "./"
+	     :publishing-directory "../html"
+	     :publishing-function 'org-html-publish-to-html
+	     :with-author nil
+	     :with-creater nil
+	     :with-toc nil
+	     :section-number nil
+	     :time-stamp-file nil
+
+)))
+
+(org-publish-all t)
+
+
+(message "Build Complete!")
