2689c2e7c8313456320a27ce5cbc2d9cfcf8066b
vaporinsky <root@ip-172-31-26-107.us-west-2.compute.internal>
2021-10-26 01:59:54 +0000

    TT

--- goG.sh
+++ goG.sh
@@ -1,6 +1,4 @@
 git add .
 git commit -m "TT"
-#git push
-git push https://vaporinsky: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@github.com\
-    /vaporinsky/vaporinsky.github.io.git --all
+git push
 
